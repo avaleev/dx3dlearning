237d9707414fd6a522163ba32e5037e6440adbaf
Anton Valeev <anton@valeev.biz>
2021-04-30 21:23:18 -0400

    Added simple frame buffer shifting

--- dx3dlearning/App.cpp
+++ dx3dlearning/App.cpp
@@ -10,13 +10,11 @@ int App::Go() {
 		}
 
 		DoFrame();
-		Sleep(1); // Temporary fix
 	}
 }
 
 void App::DoFrame() {
-	const float t = timer.Peek();
-	std::ostringstream oss;
-	oss << "Time elapsed: " << std::setprecision(1) << std::fixed << t << "s";
-	wnd.SetTitle(oss.str());
+	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
+	wnd.Gfx().ClearBuffer(c, c, 1.0f);
+	wnd.Gfx().EndFrame();
 }
--- dx3dlearning/Graphics.cpp
+++ dx3dlearning/Graphics.cpp
@@ -34,9 +34,22 @@ Graphics::Graphics(HWND hWnd) {
 		nullptr,
 		&pContext
 	);
+
+	ID3D11Resource* pBackBuffer = nullptr;
+	pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer));
+	pDevice->CreateRenderTargetView(
+		pBackBuffer,
+		nullptr,
+		&pTarget
+	);
+	pBackBuffer->Release();
 }
 
 Graphics::~Graphics() {
+	if (pTarget != nullptr) {
+		pTarget->Release();
+	}
+
 	if (pDevice != nullptr) {
 		pContext->Release();
 	}
@@ -48,4 +61,8 @@ Graphics::~Graphics() {
 	if (pDevice != nullptr) {
 		pDevice->Release();
 	}
+}
+
+void Graphics::EndFrame() {
+	pSwap->Present(1u, 0u);
 }
